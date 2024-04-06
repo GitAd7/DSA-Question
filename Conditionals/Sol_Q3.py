d2d444a2a564ff529bab7a5011191011cf708685
Adesh Singh Tomar <adesht15@gmail.com>
2024-04-06 17:00:40 +0530

    Grade Calculator

---
+++ Conditionals/Sol_Q3.py
@@ -0,0 +1,13 @@
+## Grade Calculator
+
+score = int(input('Please Mention Your Score:',))
+if 90<= score <=100:
+    print("Congratulations! You have achieved an A Grade.")
+elif 80<= score <=89:
+    print("Congratulations! You have achieved a B Grade.")
+if 70<= score <=79:
+    print("Congratulations! You have achieved a C Grade.")
+if 60<= score <=69:
+    print("Congratulations! You have achieved a D Grade.")
+if score<60:
+    print("Sorry! You have achieved an E Grade. You have to repeat this year.")
