2ad4a2b7a125e883859ce9d112b3b4e31e5bd523
Adesh Singh Tomar <adesht15@gmail.com>
2024-04-30 18:06:37 +0530

    Weather Activity Suggestion

---
+++ Conditionals/Sol_Q5.py
@@ -0,0 +1,13 @@
+## Wether based Activity Suggestor
+
+weather = str(input("Please Specify Today's Weather:"))
+if weather == "Sunny":
+    print("You can engage in any outdoor sport")
+elif weather == "Rainy":
+    print("Read a Book after checking for clothes on terrace")
+elif weather == "Windy":
+    print("Do not engage in any outdoor sport but can go for a walk or running")
+elif weather == "Snowy":
+    print("You can make a snowman and can read a book")
+else:
+    print("Please Specify the correct weather or use your eyes and brain")
