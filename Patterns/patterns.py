b163e251d77f3a49bc7d73cd5e7faa74fc2e0f65
Adesh Singh Tomar <adesht15@gmail.com>
2024-12-26 05:38:37 +0530

    Patterns Based Problem

---
+++ Patterns/patterns.py
@@ -0,0 +1,23 @@
+# This program prints a square pattern of stars based on the user input
+for i in range(n):
+    for j in range(n):
+        print('*', end='')
+    print()
+    
+# This program prints a right-angled triangle pattern of stars based on the user input
+for i in range(n):
+    for j in range(i+1):
+        print('* ',end="")
+    print()
+    
+# This program prints a right-angled triangle pattern of numbers based on the user input
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j, end=" ")
+    print()
+    
+# This program prints a right-angled triangle pattern of numbers based on the user input
+for i in range(1,n+1):
+    for j in range(i):
+        print(i,end=" ")
+    print()
