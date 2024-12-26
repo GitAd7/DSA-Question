4c4c5c03d97bbb49afe6d1ddbd8485586f73b43e
Adesh Singh Tomar <adesht15@gmail.com>
2024-12-27 04:51:24 +0530

    Pattern Based Question

--- Patterns/patterns.py
+++ Patterns/patterns.py
@@ -27,3 +27,9 @@ for i in range(1,n+1):
     for j in range(n-i+1):
         print('* ',end="")
     print()
+    
+# This program prints a right-angled triangle pattern of numbers based on the user input
+for i in range(n):
+    for j in range(1,n-i+1):
+        print(j, end=" ")
+    print()
