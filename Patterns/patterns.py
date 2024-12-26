7d0571ca1dafc50eebe80c40f4b5bd5fb0470b21
Adesh Singh Tomar <adesht15@gmail.com>
2024-12-27 05:18:09 +0530

    Pattern Based Question

--- Patterns/patterns.py
+++ Patterns/patterns.py
@@ -1,3 +1,4 @@
+n = int(input("Enter the number of rows: "))
 # This program prints a square pattern of stars based on the user input
 for i in range(n):
     for j in range(n):
@@ -32,4 +33,17 @@ for i in range(1,n+1):
 for i in range(n):
     for j in range(1,n-i+1):
         print(j, end=" ")
+    print()
+    
+# This program prints a star pattern based on the user input
+for i in range(n):
+    # For Spaces
+    for j in range(n-i-1):
+        print(' ',end="")
+    # for stars
+    for j in range(2*i+1):
+        print('*',end="")
+    # for spaces
+    for j in range(n-i-1):
+        print(' ',end="")
     print()
