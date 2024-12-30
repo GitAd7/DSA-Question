7f964c9b4f9bea04a480b476ec3646c14a6e4ccc
Adesh Singh Tomar <adesht15@gmail.com>
2024-12-30 05:44:31 +0530

    Pattern Based Question

--- Patterns/patterns.py
+++ Patterns/patterns.py
@@ -91,3 +91,11 @@ for i in range(1,n+1):
         print(j,end=" ")
         space -=2
     print()
+
+# This program prints a Increasing Number Triangle Patterns based on the user input
+num = 1
+for i in range(1,n+1):
+    for j in range(i):
+        print(num, end=" ")
+        num = num+1
+    print()
