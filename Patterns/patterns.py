4330fd36eec6d0eb96432b27b8fcc1d7cd2c6633
Adesh Singh Tomar <adesht15@gmail.com>
2025-01-02 05:14:20 +0530

    Patterns Based Question

--- Patterns/patterns.py
+++ Patterns/patterns.py
@@ -131,3 +131,10 @@ for i in range(1,n+1):
     for j in range(1,(n-i)+1):
         print(" ",end="")
     print()
+    
+# This program prints a Alpha Triangle Patterns based on the user input
+base = 65+n
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(base-j),end=" ")
+    print()
