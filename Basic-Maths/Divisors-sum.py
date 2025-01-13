2fcff7ae2dd2a1e398e8d1b0e0f8a2d49e2e92e8
Adesh Singh Tomar <adesht15@gmail.com>
2025-01-13 05:52:29 +0530

    Square-Root Approach

--- Basic-Maths/Divisors-sum.py
+++ Basic-Maths/Divisors-sum.py
@@ -13,3 +13,14 @@ def SumOfDivisors(n):
     for i in range(1, n+1):
         sum += i * (n//i)
     return sum
+# TC: O(n)
+
+# Another approach using Square root
+def SumOfDivisors(n):
+    sum = 0
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            sum += i
+            if i != n//i:
+                sum += n//i
+    return sum
